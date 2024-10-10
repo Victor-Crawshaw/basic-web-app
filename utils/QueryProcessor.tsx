94bfe781d90726f311bc0a172993d033f4ef393f
Victor Crawshaw <vcrawsha@andrew.cmu.edu>
2024-10-10 10:18:49 -0400

    prime

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -58,6 +58,28 @@ export default function QueryProcessor(query: string): string {
     const result = num1 * num2;
     return result.toString();
   }
-  
+
+  const primeMatch = normalizedQuery.match(/which of the following numbers are primes: ([\d,\s]+)/);
+  if (primeMatch) {
+    const numbers = primeMatch[1]
+      .split(',')
+      .map(num => parseInt(num.trim(), 10));
+
+    const primes = numbers.filter(isPrime);
+
+
+      return `${primes.join(', ')}`;
+
+  }
   return "";
+}
+function isPrime(num: number): boolean {
+  if (num <= 1) return false; // 0 and 1 are not prime
+  if (num <= 3) return true; // 2 and 3 are prime
+  if (num % 2 === 0 || num % 3 === 0) return false; // eliminate multiples of 2 and 3
+
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+  }
+  return true;
 }
